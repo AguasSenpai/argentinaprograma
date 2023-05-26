fc62f3f3a0a87b1f5259d52d4b884212a4d3ad29
Agustina Ojeda <magustina.ojeda25@gmail.com>
2023-05-26 17:51:29 -0300

    Actualicé resta.js y multiplicacion.js con los valores dados en el práctico, más comentarios.

--- practico0/multiplicacion.js
+++ practico0/multiplicacion.js
@@ -3,3 +3,5 @@ let num2 = 17;
 let num3 = 6;
 let multiplicacion = num1 * num2 * num3;
 console.log(multiplicacion);
+
+// Si los nros. a multiplicar son 2, 17 y 6, el programa arroja como resultado 204.
--- practico0/resta.js
+++ practico0/resta.js
@@ -2,3 +2,5 @@ let num1 = 10;
 let num2 = 2;
 let resta = num1 - num2;
 console.log(resta);
+
+// Si el minuendo es 10 y el sustraendo 2, el programa devuelve 8 como resultado.
