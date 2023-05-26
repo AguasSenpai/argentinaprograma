54af7c2196c6de71c6a3f615baec43650d81d8ad
Agustina Ojeda <magustina.ojeda25@gmail.com>
2023-05-26 18:13:19 -0300

    Actualicé areaRectangulo.js con los valores dados en el práctico, más comentarios.

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,7 @@
+let base = 12.5;
+let altura = 5.3;
+let area = base * altura;
+console.log(area);
+
+// Si tiene base de 5 cm y altura de 3 cm, el área es de 15 cm^2.
+// Si tiene base de 12,5 cm y altura de 5,3 cm, el área es de 66,25 cm^2.
