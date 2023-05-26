ab0b4034f401c026edccf0fdb681403a6d0718fe
Agustina Ojeda <magustina.ojeda25@gmail.com>
2023-05-26 18:35:12 -0300

    Actualicé promedio3.js con los valores dados en el práctico, más comentarios. Práctico N0 finalizado.

---
+++ practico0/promedio3.js
@@ -0,0 +1,9 @@
+let num1 = -1.2;
+let num2 = 5.3;
+let num3 = -3.3;
+let promedio = (num1 + num2 + num3) / 3;
+console.log(promedio);
+
+// El promedio de 5, 7 y 9 es igual a 7.
+// El promedio de -2, -3 y -8 es igual a -4,3333...
+// El promedio de -1.2, 5.3 y -3.3 es igual a 0.2666...
