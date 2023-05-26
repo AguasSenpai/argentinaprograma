54469957288aecff66cfbbe3dd83c1d4971a1797
Agustina Ojeda <magustina.ojeda25@gmail.com>
2023-05-26 18:20:18 -0300

    Actualicé conversionGrados.js con los valores dados en el práctico, más comentarios.

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,6 @@
+let fahrenheit = 75;
+let celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);
+
+// 50 grados Fahrenheit son 10 grados Celsius.
+// 75 grados Fahrenheit son 23,8888... grados Celsius.
