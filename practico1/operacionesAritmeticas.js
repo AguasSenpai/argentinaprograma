9bccd31f3ee799be794d84bd8a4d7e9b09efdda6
Agustina Ojeda <magustina.ojeda25@gmail.com>
2023-06-12 00:09:59 -0300

    Se agrega el programa operacionesAritmeticas.js.

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,9 @@
+let var1 = 3;
+let var2 = 1;
+let var3 = 17;
+let var4 = 6;
+let sumaVar = var1 + var2;
+let restaVar = var3 - var4;
+let resultadoFinal = sumaVar * restaVar;
+let esPar = (resultadoFinal % 2 === 0);
+console.log("Mis variables iniciales fueron: %i, %i, %i y %i. La respuesta a verificar si el resultado final es par es: %s.", var1, var2, var3, var4, esPar);
