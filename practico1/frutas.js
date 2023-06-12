62e8ec7871b80c56eab690ae4e15cfae0992ecd4
Agustina Ojeda <magustina.ojeda25@gmail.com>
2023-06-12 01:15:17 -0300

    Se agrega el programa frutas.js. Práctico N1 finalizado.

---
+++ practico1/frutas.js
@@ -0,0 +1,8 @@
+let frutas = [];
+frutas.push("manzana", "pera", "banana", "kiwi", "durazno");
+
+let ultima = frutas[4];
+console.log("La última fruta agregada fue: %s.", ultima);
+
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: %s.", primera);
