9def1fbfee5d9cd58181382f7db761fb02df88c9
Agustina Ojeda <magustina.ojeda25@gmail.com>
2023-06-17 21:16:39 -0300

    Se agrega programa de enunciado 1b.

---
+++ practico2/positivoNegativo.js
@@ -0,0 +1,12 @@
+const readlineSync = require('readline-sync');
+const numero = readlineSync.question('Inserte un numero: ');
+
+if (numero > 0) {
+    console.log('El numero es positivo.');
+} else {
+    if (numero == 0) {
+        console.log('El numero es cero.');
+    } else {
+        console.log('El numero es negativo.');
+    }
+};
