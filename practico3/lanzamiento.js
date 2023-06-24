ab7c13ac060d60f1913f6bf23924f6f29aa7c4e6
Agustina Ojeda <magustina.ojeda25@gmail.com>
2023-06-24 01:39:54 -0300

    Se agrega programa de cuenta regresiva.

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,15 @@
+let i = 10;
+let j = 0;
+
+do {
+    if (i === 10) {
+        console.log('Cuenta regresiva ' + i);
+    } else {
+        console.log(i);
+    }
+    i--;
+} while (i > j);
+
+if (i === j) {
+    console.log('Lanzamiento!');
+}
